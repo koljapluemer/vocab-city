9de3320b2d052f4262a8cf1a3b989ed14fa4f642
blackbrokkoli <kastomat@gmail.com>
2024-03-24 12:20:54 +0100

    add basic clickable tilemap

--- scenes/map.gd
+++ scenes/map.gd
@@ -1,9 +1,12 @@
 extends TileMap
 
+func _ready():
+	print("TileMap ready")
+
 func _physics_process(_delta):
 	if (Input.is_action_just_pressed("mb_left")):
 		var tile: Vector2 = local_to_map(get_global_mouse_position())
-		set_cell(0, tile, 0)
+		set_cell(0, tile, 0, Vector2(0, 0))
 
 	if (Input.is_action_just_pressed("mb_right")):
 		var tile: Vector2 = local_to_map(get_global_mouse_position())
