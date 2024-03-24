424ef8374c4ed3d70d5440334790e4ffc319c2ce
blackbrokkoli <kastomat@gmail.com>
2024-03-24 12:56:53 +0100

    add text on top of tile

--- scenes/map.gd
+++ scenes/map.gd
@@ -1,5 +1,7 @@
 extends TileMap
 
+var targetLabelPrefab = load("res://scenes/prefabs/TargetLabel.tscn")
+
 func _ready():
 	print("TileMap ready")
 
@@ -7,10 +9,11 @@ func _physics_process(_delta):
 	if (Input.is_action_just_pressed("mb_left")):
 		var tile: Vector2 = local_to_map(get_global_mouse_position())
 		set_cell(0, tile, 0, Vector2(0, 0))
+		var targetLabel = targetLabelPrefab.instantiate()
+		targetLabel.set_position(map_to_local(tile))
+		targetLabel.text = "mouse"
+		add_child(targetLabel)
 
-	if (Input.is_action_just_pressed("mb_right")):
-		var tile: Vector2 = local_to_map(get_global_mouse_position())
-		set_cell(0, tile, 1)
 
 		
 		
