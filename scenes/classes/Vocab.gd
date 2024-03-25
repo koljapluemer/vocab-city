e777329bb05614b646ed600972b08c5b7555b9d2
blackbrokkoli <kastomat@gmail.com>
2024-03-25 11:40:27 +0100

    use class to build map tile

--- scenes/classes/Vocab.gd
+++ scenes/classes/Vocab.gd
@@ -2,3 +2,11 @@ class_name Vocab
 
 var native_word: String;
 var target_word: String;
+var x: int;
+var y: int;
+
+func _init(in_native_word, in_target_word, in_x, in_y):
+	native_word = in_native_word
+	target_word = in_target_word
+	x = in_x
+	y = in_y
--- scenes/map.gd
+++ scenes/map.gd
@@ -8,10 +8,13 @@ func _ready():
 func _physics_process(_delta):
 	if (Input.is_action_just_pressed("mb_left")):
 		var tile: Vector2 = local_to_map(get_global_mouse_position())
-		set_cell(0, tile, 0, Vector2(0, 0))
+		
+		var vocab = Vocab.new("Maus", "Mouse", tile.x, tile.y)
+		
+		set_cell(0, Vector2(vocab.x, vocab.y), 0, Vector2(0, 0))
 		var targetLabel = targetLabelPrefab.instantiate()
 		targetLabel.set_position(map_to_local(tile))
-		targetLabel.text = "mouse"
+		targetLabel.text = vocab.target_word
 		add_child(targetLabel)
 
 
